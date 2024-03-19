036eee7b657e47031595d27193879130448be166
DilipWebPandit <dilip7065kumar@gmail.com>
2024-03-19 23:12:35 +0530

    This is basic calculator

---
+++ index.js
@@ -0,0 +1,6 @@
+function workCheck(){
+    console.log("This is working");
+}
+var link = document.getElementsByClassName("lin1")[0].addEventListener("click", workCheck);
+var a = document.getElementById("serach").innerText = link;
+document.write(a)
